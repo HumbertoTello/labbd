304431e45b66f65db35344cade2e9ea4e5f5fcc3
Humberto <betoelo2@gmail.com>
2023-06-15 11:59:24 -0300

    Project prototype

---
+++ create_table_users.sql
@@ -0,0 +1,6 @@
+-- Criação de protótipo da tabela apenas para testes de interface, não é a tabela final
+-- Rodar a query abaixo no seu servidor de banco de dados para que o código main.py funcione corretamente
+CREATE TABLE users (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL
+);
