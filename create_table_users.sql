c80dc5baa2773eb9a74c95992548b5f6566ac3f4
Humberto <betoelo2@gmail.com>
2023-06-21 09:06:51 -0300

    Trigger update to cancel duplicates

--- create_table_users.sql
+++ create_table_users.sql
@@ -12,16 +12,23 @@ CREATE TABLE users (
 modificado, o registro na tabela USERS ser corrigido também */
 CREATE OR REPLACE FUNCTION update_user_on_driver_insert()
 RETURNS TRIGGER AS $$
+DECLARE
+    existing_driverref INTEGER;
 BEGIN
-    INSERT INTO users (Login, Password, Tipo, IdOriginal)
-    VALUES (NEW.DriverRef || '_d', NEW.DriverRef, 'Piloto', NEW.DriverId);
-
+    SELECT COUNT(*) INTO existing_driverref FROM Driver WHERE DriverRef = NEW.DriverRef;
+    IF existing_driverref = 0 THEN
+        INSERT INTO users (Login, Password, Tipo, IdOriginal)
+        VALUES (NEW.DriverRef || '_d', NEW.DriverRef, 'Piloto', NEW.DriverId);
+    ELSE
+        RAISE EXCEPTION 'A referência do Piloto fornecida já existe. Cadastro cancelado.';
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS driver_insert ON Driver;
 CREATE TRIGGER driver_insert
-AFTER INSERT ON Driver
+BEFORE INSERT ON Driver
 FOR EACH ROW
 EXECUTE PROCEDURE update_user_on_driver_insert();
 
@@ -29,16 +36,23 @@ EXECUTE PROCEDURE update_user_on_driver_insert();
 modificada, o registro na tabela USERS ser corrigido também */
 CREATE OR REPLACE FUNCTION update_user_on_constructor_insert()
 RETURNS TRIGGER AS $$
+DECLARE
+    existing_constructorref INTEGER;
 BEGIN
-    INSERT INTO users (Login, Password, Tipo, IdOriginal)
-    VALUES (NEW.ConstructorRef || '_c', NEW.ConstructorRef, 'Escuderia', NEW.ConstructorId);
-
+    SELECT COUNT(*) INTO existing_constructorref FROM Constructors WHERE ConstructorRef = NEW.ConstructorRef;
+    IF existing_constructorref = 0 THEN
+        INSERT INTO users (Login, Password, Tipo, IdOriginal)
+        VALUES (NEW.ConstructorRef || '_c', NEW.ConstructorRef, 'Escuderia', NEW.ConstructorId);
+    ELSE
+        RAISE EXCEPTION 'A referência da Escuderia fornecida já existe. Cadastro cancelado.';
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS constructor_insert ON Constructors;
 CREATE TRIGGER constructor_insert
-AFTER INSERT ON Constructors
+BEFORE INSERT ON Constructors
 FOR EACH ROW
 EXECUTE PROCEDURE update_user_on_constructor_insert();
 
